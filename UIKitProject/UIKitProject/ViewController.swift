8e14e545d6278fe28738895f253536baa21ebe87
Arthur111 Magomedov <arthur111magomedov@MacBook-Air-Arthur111.local>
2022-03-31 17:51:58 +0300

    init

---
+++ UIKitProject/UIKitProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitProject
+//
+//  Created by Arthur111 Magomedov on 31.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
